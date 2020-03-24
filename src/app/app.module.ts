d778bae0aabf03023559e0b16a00552ff8a1bb38
shemeenarahim1997 <sshemeena123@gmail.com>
2020-03-24 16:20:21 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { PageheaderComponent } from './layout/pageheader/pageheader.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
+import { InviteComponent } from './pages/invite/invite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    LayoutComponent,
+    PageheaderComponent,
+    SidebarComponent,
+    PagesComponent,
+    CreateSurveyComponent,
+    InviteComponent
   ],
   imports: [
     BrowserModule,
